96ec34bb3afcbf80776d021d1919d32dd35bd38a
Matthias Buchholz <matthias@afkm.io>
2017-09-13 20:54:30 +0200

    export interfaces

--- src/module.ts
+++ src/module.ts
@@ -2,6 +2,10 @@ import { CommonModule } from '@angular/common'
 import { NgModule, ModuleWithProviders, Provider } from '@angular/core'
 import { ScrollService } from './services/scroll.service'
 
+export { ScrollEvent, ScrollMarginUpdate } from './interfaces/scroll-event'
+export { ScrollMargin, ScrollPosition, ScrollState, ScrollableComponent, ScrollableItem, isScrollableComponent } from './interfaces/scrollable-component'
+export { ScrollService } from './services/scroll.service'
+
 @NgModule({
   imports: [CommonModule],
   //declarations: [],
